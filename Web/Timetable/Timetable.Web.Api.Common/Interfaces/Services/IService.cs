f2e973c1a698a3572c88bebdb9a95856bb2af655
Matt Collecutt <slothpie@outlook.com>
2017-09-12 09:30:59 +1200

    adding classes to api common

---
+++ Web/Timetable/Timetable.Web.Api.Common/Interfaces/Services/IService.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+namespace Timetable.Web.Api.Common.Interfaces.Services
+{
+    public interface IService<out T>
+    {
+        IQueryable<T> All();
+        T GetById(long id);
+        bool Delete(long id);
+    }
+}
---
+++ Web/Timetable/Timetable.Web.Api.Common/Services/Service.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Timetable.Web.Api.Common.Interfaces.Services;
+using Timetable.Web.Api.Common.Interfaces.Repositories;
+
+namespace Timetable.Web.Api.Common.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        public Service(IRepository<T> repository)
+        {
+            Repository = repository;
+        }
+
+        protected IRepository<T> Repository { get; set; }
+
+        public bool Delete(long id)
+        {
+            return Repository.Delete(id);
+        }
+
+        public IQueryable<T> All()
+        {
+            return Repository.All();
+        }
+
+        public T GetById(long id)
+        {
+            return Repository.GetById(id);
+        }
+    }
+}
