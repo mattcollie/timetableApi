d5b50bde68be4d401d72b461409ced92b9206a62
Matt Collecutt <slothpie@outlook.com>
2017-09-12 08:41:06 +1200

    adding all base projects

---
+++ Web/Timetable/Timetable.Web.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Timetable.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Web/Timetable/Timetable.Web.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Timetable.Web.Api.WebApiApplication" Language="C#" %>
