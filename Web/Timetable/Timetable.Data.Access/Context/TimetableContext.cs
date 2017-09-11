6569f9200dc4992643e780ab35af6bc82c8a868e
Matt Collecutt <slothpie@outlook.com>
2017-09-12 08:58:23 +1200

    adding context classes

---
+++ Web/Timetable/Timetable.Data.Access/Context/TimetableContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Timetable.Common.BaseEntities;
+
+namespace Timetable.Data.Access.Context
+{
+    [DbConfigurationType(typeof(TimetableContextConfiguration)]
+    public class TimetableContext : DbContext
+    {
+        public TimetableContext() : base("name=DefaultConnection")
+        {
+
+        }
+
+        #region Overrides
+
+        public static TimetableContext Create()
+        {
+            return new TimetableContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+
+        public new virtual IDbSet<T> Set<T>() where T : class
+        {
+            return base.Set<T>();
+        }
+
+        public override int SaveChanges()
+        {
+            AddTimestamps();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync()
+        {
+            AddTimestamps();
+            return await base.SaveChangesAsync();
+        }
+
+        private void AddTimestamps()
+        {
+            foreach (var entity in ChangeTracker.Entries()
+                .Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified)))
+            {
+                DateTime now = DateTime.UtcNow;
+                if (entity.State == EntityState.Added)
+                {
+                    ((BaseEntity)entity.Entity).CreatedAt = now;
+                }
+                ((BaseEntity)entity.Entity).UpdatedAt = now;
+            }
+        }
+
+        #endregion
+
+        #region Entities
+        
+
+        #endregion
+    }
+}
---
+++ Web/Timetable/Timetable.Data.Access/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity.Migrations;
+using Timetable.Data.Access.Context;
+
+namespace Timetable.Data.Access.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<TimetableContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        private TimetableContext Context { get; set; }
+
+        protected override void Seed(TimetableContext context)
+        {
+            Context = context;
+
+            Context.SaveChanges();
+        }
+
+        #region Seeds
+
+        
+
+        #endregion
+    }
+}
